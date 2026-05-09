5b8a2023a0d9bb158a105b608ef490f051b2a70f
vescentongit <rohankishibe441@gmail.com>
2026-05-09 12:28:10 +0700

    feat:db can read env variable

--- src/backend/ai/database.py
+++ src/backend/ai/database.py
@@ -1,15 +1,15 @@
-# database.py
-from dotenv import load_dotenv
-load_dotenv()
+# ai/database.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
